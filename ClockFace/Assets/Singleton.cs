1324c8c376a2df1a2b3eabb7a485c07744609f39
7kia <il7kia@yandex.ru>
2017-05-15 22:07:50 +0300

    Add slots

---
+++ ClockFace/Assets/Singleton.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+
+//public class Singleton<T>
+//{
+//    private static Singleton<T> instance;
+
+//    private Singleton<T>() { }
+
+//    public static Singleton<T> Instance
+//    {
+//        get
+//        {
+//            if (instance == null)
+//            {
+//                instance = new Singleton<T>();
+//            }
+//            return instance;
+//        }
+//    }
+//}
