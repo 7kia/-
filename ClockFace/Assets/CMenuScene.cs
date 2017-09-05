cc0f2064afb0895b3e899df56392380a10aed351
7kia <il7kia@yandex.ru>
2017-09-05 18:40:16 +0400

    Add MenuScene, RecordTableScene and part transitions between it's

---
+++ ClockFace/Assets/CMenuScene.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CMenuScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadRecordTable()
+    {
+        SceneManager.LoadScene("RecordTableScene", LoadSceneMode.Single);
+    }
+
+    public void StartNewGame()
+    {
+        // TODO : reset info, load 1 level
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+    }
+}
---
+++ ClockFace/Assets/LevelManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    class LevelInfo
+    {
+        int edgeNumber = 4;
+        int countNumber = 3;
+        int amountNumberToEdge = 9;
+    }
+
+    private List<LevelInfo> m_levelInfos = new List<LevelInfo>();
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadLevel(int levelId)
+    {
+        if (levelId != 1)
+        {
+            throw new Exception("levelId != 1");
+        }
+
+        // TODO : add new levels
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+    }
+
+}
